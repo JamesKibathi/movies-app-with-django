12e85de6984631591a9becdad57fbe178a2b4ad2
James Njenga <knjenga94@gmail.com>
2023-04-25 22:16:58 +0300

    Create first template and views

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+data ={
+    'movies':[
+        {
+            "id":5,
+            "title":"Olympus has fallen",
+            "year":2012
+            
+        },
+        {
+             "id":2,
+            "title":"Dynasty",
+            "year":2008
+        },
+        {
+             "id":3,
+            "title":"Wednesday",
+            "year":2022
+        }
+       
+        ]
+}
 
 def movies(request):
-    return HttpResponse("Hello there")
+    return render(request,'movies/movies.html',data)
 
 def home(request):
     return HttpResponse("Home page")
